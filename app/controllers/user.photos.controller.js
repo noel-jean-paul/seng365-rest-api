4089ac9330dcc9c3c26eb0c6f66eecdd8b7c0312
Noel Bisson <nbi21@uclive.ac.nz>
2019-03-20 08:51:59 +1300

    Implement user photo put (no database save)

--- app/controllers/user.photos.controller.js
+++ app/controllers/user.photos.controller.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const UserPhotos = require('../models/user.photos.model');
+const User = require('../models/user.model');
 const userPhotoUtils = require('../utils/user.photos');
+
 const auth = require('../utils/auth');
 const fs = require('fs');
+
+const basePath = 'app/assets/users/photos/';
 
 
 exports.retrieve = async (req, res) => {
@@ -15,20 +19,40 @@ exports.set = async (req, res) => {
     const userId = req.params.userId;
 
     // Validation
-    if (userId !== auth.getAuthenticatedUserId()) {
+    if (!User.checkUserExists(userId)) {
+        res.statusMessage = 'Not Found';
+        return res.status(404)
+            .send();
+    } else if (userId !== auth.getAuthenticatedUserId()) {
         res.statusMessage = "Forbidden";
         return res.status(403)
             .send();
+    } else if (!image) {
+        res.statusMessage = 'Bad Request';
+        return res.status(400)
+            .send();
+    }
+
+    // save photo
+    const path = `${basePath}${userId}`;
+    if (fs.existsSync(path)) {
+        res.statusMessage = 'OK';
+        res.status(200);
+    } else {
+        res.statusMessage = 'Created';
+        res.status(201);
     }
 
     try {
-        await fs.writeFile(`assets/users/photos/${userId}`, image);
+        fs.writeFileSync(path, image);
         // save the filepath in the database
+        res.send();
     } catch(err) {
-        console.error(err);
+        if (!err.hasBeenLogged) console.error(err);
+        res.statusMessage = 'Internal server error';
+        return res.status(500)
+            .send();
     }
-    res.status(201)
-        .send();
 };
 
 exports.remove = async (req, res) => {
