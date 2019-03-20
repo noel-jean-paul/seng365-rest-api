cac308be95a9965581f7f9bd71414a27eb607b38
Noel Bisson <nbi21@uclive.ac.nz>
2019-03-21 12:56:51 +1300

    Implement POST/venues -> failing 201 test due to date mismatch

--- app/controllers/venue.controller.js
+++ app/controllers/venue.controller.js
@@ -14,5 +14,22 @@ exports.create = async (req, res) => {
         res.statusMessage = "Bad Request: " + errorMsg;
         return res.status(400)
             .send();
+    } else if (! (await Venue.getAllCategoryIds()).includes(req.body.categoryId)) {
+        res.statusMessage = 'Bad Request: categoryId does not match any existing category';
+        return res.status(400)
+            .send();
+    }
+
+    const userId = auth.getAuthenticatedUserId();
+    try {
+        const venueId = await Venue.insert(userId, req.body);
+        res.statusMessage = 'Created';
+        return res.status(201)
+            .json({"venueId": venueId});
+    } catch (err) {
+        if (!err.hasBeenLogged) console.error(err);
+        res.statusMessage = 'Internal server error';
+        return res.status(500)
+            .send();
     }
 };
--- app/models/venue.model.js
+++ app/models/venue.model.js
@@ -1,5 +1,44 @@
 'use strict';
 
+const moment = require('moment');
+
 const db = require('../../config/db');
-const venueUtils = require('../utils/venue');
-const auth = require('../utils/auth');
+
+//
+// Internal helper functions
+//
+exports.getAllCategoryIds = async () => {
+    const sql = 'SELECT category_id FROM VenueCategory';
+
+    try {
+        const rows = await db.getPool().query(sql);
+        return rows.map(row => row.category_id);
+    } catch (err) {
+        throw err;
+    }
+};
+
+
+//
+// 'Main' functions
+//
+
+exports.insert = async (userId, data) => {
+    const sql = "INSERT INTO Venue (admin_id, category_id, venue_name, city, short_description," +
+        "long_description, date_added, address, latitude, longitude) VALUES (?)";
+
+    let dateAdded = moment.utc().format('YYYY-MM-DDT00:00:00.000') + 'Z';
+    console.log(dateAdded);
+
+    const values = [
+        [userId, data.categoryId, data.venueName, data.city, data.shortDescription, data.longDescription,
+        dateAdded, data.address, data.latitude, data.longitude]
+    ];
+
+    try {
+        const result = await db.getPool().query(sql, values);
+        return result.insertId;
+    } catch(err) {
+        throw err;
+    }
+};
