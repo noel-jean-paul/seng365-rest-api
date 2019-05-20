e5e95970c5f537f7df5e5acbff5de6a7883629ed
Noel Bisson <nbi21@uclive.ac.nz>
2019-05-20 16:47:22 +1200

    M16 - photo upload - Add photo modal

--- chat-app/src/Venues/SingleVenue/VenuePhotos.vue
+++ chat-app/src/Venues/SingleVenue/VenuePhotos.vue
@@ -2,58 +2,72 @@
   <div id="venuePhotos" v-if="venue">
     <div v-if="venue.photos.length > 0">
 
-    <v-layout>
-      <v-flex offset-sm3>
-        <v-card>
+      <v-layout>
+        <v-flex offset-sm3>
+          <v-card>
 
 
-          <v-container grid-list-sm fluid>
-            <v-layout row wrap>
-              <v-flex
-                v-for="(photo, index) of venue.photos"
-                :key="index"
-                xs4
-                d-flex
-              >
-                <v-card flat tile class="d-flex">
-                  <v-img
-                    :src="$baseUrl + '/venues/' + venue.venueId + '/photos/' +
+            <v-container grid-list-sm fluid>
+              <v-layout row wrap>
+                <v-flex
+                  v-for="(photo, index) of venue.photos"
+                  :key="index"
+                  xs4
+                  d-flex
+                >
+                  <v-card flat tile class="d-flex">
+                    <div style="cursor: pointer" @click="() => onPhotoClick(photo)">
+                      <v-img
+                        :src="$baseUrl + '/venues/' + venue.venueId + '/photos/' +
               photo.photoFilename"
-                    :alt="photo.photoDescription"
-                    aspect-ratio="1"
-                    :class="{'border border-primary': photo.isPrimary}"
-                  >
-                    <template v-slot:placeholder>
-                      <v-layout
-                        fill-height
-                        align-center
-                        justify-center
-                        ma-0
+                        :alt="photo.photoDescription"
+                        aspect-ratio="1"
+                        :class="{'border border-primary': photo.isPrimary, 'border-3': true}"
                       >
-                        <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-                      </v-layout>
-                    </template>
-                  </v-img>
-                </v-card>
-              </v-flex>
-            </v-layout>
+                        <template v-slot:placeholder>
+                          <v-layout
+                            fill-height
+                            align-center
+                            justify-center
+                            ma-0
+                          >
+                            <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                          </v-layout>
+                        </template>
+                      </v-img>
+                    </div>
+                  </v-card>
 
-          </v-container>
+                  <b-modal v-model="showPhotoModal">
+                    <b-container>
+                      <v-img :src="$baseUrl + '/venues/' + venue.venueId + '/photos/' +
+              photo.photoFilename"
+                             :alt="photo.photoDescription"/>
 
-        </v-card>
+                    </b-container>
+                  </b-modal>
 
-        <PhotoUpload class="mt-2" @upload="$emit('upload')" :hasPhotos="venue.photos.length > 0"/>
+                </v-flex>
+              </v-layout>
 
-      </v-flex>
-    </v-layout>
+            </v-container>
+
+          </v-card>
+
+          <PhotoUpload class="mt-2" @upload="$emit('upload')" :hasPhotos="venue.photos.length > 0"/>
+
+        </v-flex>
+      </v-layout>
+
+    </div>
+    <div v-else>
+      <h3> This venue has no photos </h3>
+      <PhotoUpload class="mt-2" @upload="$emit('upload')" :hasPhotos="venue.photos.length > 0"/>
+    </div>
 
   </div>
-  <div v-else>
-    <h3> This venue has no photos </h3>
-    <PhotoUpload class="mt-2" @upload="$emit('upload')" :hasPhotos="venue.photos.length > 0"/>
-  </div>
 
-  </div>
+
 </template>
 
 <script>
@@ -62,16 +76,32 @@
   export default {
     name: "VenuePhotos",
 
+    data() {
+      return {
+        showPhotoModal: false
+      }
+    },
+
     props: {
       venue: Object
     },
 
     components: {
       PhotoUpload
+    },
+
+    methods: {
+      onPhotoClick(photo) {
+        console.log('clicky click click');
+        this.showPhotoModal = true;
+      }
     }
   }
 </script>
 
 <style scoped>
+  .border-3 {
+    border-width:5px !important;
+  }
 
 </style>
